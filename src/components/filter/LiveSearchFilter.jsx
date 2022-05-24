d15703fb025eaef860243ca70f0c47957e0bcf95
Ole994 <69842859+Ole994@users.noreply.github.com>
2022-05-24 18:47:09 +0200

    searchbar started

--- src/components/filter/LiveSearchFilter.jsx
+++ src/components/filter/LiveSearchFilter.jsx
@@ -1,9 +1,50 @@
-import { Component } from "react";
-import axios from "axios";
-import { BASE_URL, getData } from "../../helpers/api/api";
-import { useEffect, useState } from "react";
+import styled from "styled-components";
 
-const LiveSearchFilter = () => {
-  return <div>jh</div>;
+const SearchBarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 34em;
+  height: 3.8em;
+  background-color: white;
+  border-radius: 6px;
+  box-shadow: 0px 2px 12px 3px rgba(0, 0, 0, 0.14);
+`;
+
+const searchInputContainer = styled.div`
+  width: 100%;
+  min-height: 4em;
+  display: flex;
+  align-items: center;
+  position: relative;
+  padding: 2px 15px;
+`;
+
+const searchInput = styled.div`
+  width: 100%;
+  height: 100%;
+  outline: none;
+  border: none;
+  font-size: 21px;
+  color: #12112e;
+  font-weight: 500;
+  border-radius: 6px;
+  background-color: transparent;
+
+  &:focus {
+    outline: none;
+    &::placeholder {
+      color: #bebebe;
+    }
+  }
+`;
+
+const LiveSearchFilter = (props) => {
+  return (
+    <SearchBarContainer>
+      <searchInputContainer>
+        <searchInput />
+      </searchInputContainer>
+    </SearchBarContainer>
+  );
 };
 export default LiveSearchFilter;
